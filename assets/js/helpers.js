dbf6a3c08181c38ead1801ab959772f4d74d8d2c
ProtocolNebula <powergm@hotmail.es>
2018-10-10 00:31:29 +0200

    Test ajax

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -63,3 +63,27 @@ function decodeURI(string) {
 function getBaseURL() {
     return window.location.origin + window.location.pathname;
 }
+
+/**
+ * Get a remote file via GET using AJAX
+ * @link https://stackoverflow.com/questions/6150289/how-to-convert-image-into-base64-string-using-javascript
+ * @param {*} url 
+ * @param {*} callback Function to process the result
+ */
+function toDataURL(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+        callback(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+}
+
+toDataURL('https://www.gravatar.com/avatar/d50c83cc0c6523b4d3f6085295c953e0', function(dataUrl) {
+console.log('RESULT:', dataUrl)
+})
